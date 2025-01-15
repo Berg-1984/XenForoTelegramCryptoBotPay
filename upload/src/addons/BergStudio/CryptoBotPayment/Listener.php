2107b5b3935a121c955882b63131cabb2c62368b
Berg-1984 <bergstudio.net@gmail.com>
2025-01-15 13:26:56 +0200

    Add files via upload

---
+++ upload/src/addons/BergStudio/CryptoBotPayment/Listener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BergStudio\CryptoBotPayment;
+
+use XF\Mvc\Entity\Entity;
+
+class Listener
+{   
+  public static function postSavePurchaseRequest(\XF\Mvc\Entity\Entity $entity)
+  { 	  
+      if($entity->provider_id !== 'tg_crypto_bot')
+      {
+        return;
+      }
+      
+      $options = $entity->PaymentProfile->options;    
+      $fee = intval($options['cb_fee']);  
+      
+      if($fee > 0)
+      {
+       $cost_fee_amount = $entity->cost_amount + ($entity->cost_amount * $fee / 100);
+       
+       $entity->set('cost_amount', $cost_fee_amount, ['forceSet' => true]); 
+      } 
+  }
+}
+ 
