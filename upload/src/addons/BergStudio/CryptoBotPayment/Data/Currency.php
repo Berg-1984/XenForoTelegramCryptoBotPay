7bef40a1f0126429a852259e060094107400e442
Berg-1984 <bergstudio.net@gmail.com>
2025-02-06 16:27:21 +0200

    Add files via upload

---
+++ upload/src/addons/BergStudio/CryptoBotPayment/Data/Currency.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BergStudio\CryptoBotPayment\Data;
+
+class Currency
+{
+   public function fiat()
+   {
+    return [
+            'USD', 'EUR', 'RUB', 'BYN', 'UAH', 'GBP', 'CNY',
+            'KGS', 'TJS', 'KZT', 'UZS', 'GEL', 'TRY', 'AMD',
+            'THB','INR', 'BRL', 'IDR', 'AZN', 'AED', 'PLN', 'ILS'
+           ];
+   }
+
+   public function crypto()
+   {
+    return [
+            'USDT', 'TON', 'BTC', 'ETH', 'LTC', 'BNB', 'DOGE',
+            'TRX', 'USDC', 'SOL', 'GRAM', 'NOT', 'HMSTR', 'DOGS',
+            'CATI', 'PEPE', 'WIF', 'BONK','MAJOR', 'MY'
+           ];
+   }
+
+   public function isSupportedCurrency($currencyCode)
+   {
+    return in_array(strtoupper($currencyCode), $this->fiat());
+   }
+
+}
